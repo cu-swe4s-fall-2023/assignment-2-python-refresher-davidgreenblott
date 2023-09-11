2e615a4cd529776896e4e8eaaead4362d9327bff
davidgreenblott <david.greenblot@colorado.edu>
2023-09-11 21:11:28 +0000

    implement print_fires and update my_utils

--- my_utils.py
+++ my_utils.py
@@ -1,23 +1,20 @@
 
+#takes in a file name and returns values in a column that match a query_value
 def get_column(file_name, query_column, query_value, result_column):
     
     matchingValues = []
-    #this makes sure that we close the file without the close command
+    
+    #open file robustly
     with open(file_name,'r') as f:
         
         for line in f:
             
-            # print(line)
             items = line.strip().split(',')
-            # data.append(items)
-            # if items[1]=='1990':
-            #     print('here')
-           
-            # print(items[query_column])
+            
+            #adds result column of interest to return block 
+            #if query_column value matches query_value
             if items[query_column] == str(query_value):
-                # print('here')
+
                 matchingValues.append(items[result_column])
             
     return matchingValues
-
-print(get_column('Agrofood_co2_emission.csv',1,1990,3))
--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,15 @@
+from my_utils import get_column
+
+#print the total number of files in a specified country
 country='United States of America'
-county_column = 1
-fires_column = 4
+country_column = 0
+fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
-print(fires)
+fires = get_column(file_name,country_column,country,fires_column)
+
+totalFires = 0
+
+for fire in fires:
+    totalFires += float(fire)
+print(f'Total Fires in {country} between 1990 and 2020: {totalFires}')
+# print(sum(int(fires)))
