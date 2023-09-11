f63b5aae4e28f694c5022830e87e5877ea43ff75
davidgreenblott <david.greenblot@colorado.edu>
2023-09-11 20:55:50 +0000

    implement get_column

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,23 @@
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    
+    matchingValues = []
+    #this makes sure that we close the file without the close command
+    with open(file_name,'r') as f:
+        
+        for line in f:
+            
+            # print(line)
+            items = line.strip().split(',')
+            # data.append(items)
+            # if items[1]=='1990':
+            #     print('here')
+           
+            # print(items[query_column])
+            if items[query_column] == str(query_value):
+                # print('here')
+                matchingValues.append(items[result_column])
+            
+    return matchingValues
+
+print(get_column('Agrofood_co2_emission.csv',1,1990,3))
