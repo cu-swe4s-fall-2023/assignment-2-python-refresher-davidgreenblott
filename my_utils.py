a1226ebd969993554f32c8c0301bb5871d189e5b
davidgreenblott <david.greenblot@colorado.edu>
2023-09-27 02:52:35 +0000

    accept cmd args

--- my_utils.py
+++ my_utils.py
@@ -1,20 +1,40 @@
+'''takes in a file name and returns values
+in a column that match a query_value'''
 
-#takes in a file name and returns values in a column that match a query_value
-def get_column(file_name, query_column, query_value, result_column = 1):
-    
+import sys
+
+
+def get_column(file_name, query_column, query_value, result_column=1):
+    '''Returns a list of values from the query inputs
+    Parameters
+    ----------
+
+    file_name: string
+               name of file to access
+    query_column: int
+                  column to perform query on
+    query_value: int
+                 which country to perform wserach on
+    resut_column: int
+                  the column that has value of interest
+    '''
     matchingValues = []
-    
-    #open file robustly
-    with open(file_name,'r') as f:
-        
-        for line in f:
-            
-            items = line.strip().split(',')
-            
-            #adds result column of interest to return block 
-            #if query_column value matches query_value
-            if items[query_column] == str(query_value):
 
-                matchingValues.append(items[result_column])
-            
+    try:
+        with open(file_name, 'r') as f:
+
+            for line in f:
+
+                items = line.strip().split(',')
+
+                if items[query_column] == str(query_value):
+
+                    matchingValues.append(items[result_column])
+    except FileNotFoundError:
+        print('Could not find ' + file_name)
+        sys.exit(1)
+    except PermissionError:
+        print('Could not open ' + file_name)
+        sys.exit(1)
+
     return matchingValues
--- print_fires.py
+++ print_fires.py
@@ -1,15 +1,53 @@
+'''Prints out the total number of fires for a given country
+    * get_user_args() - accepts user input from command line
+    * main() - calls get_column() to find query result'''
+
 from my_utils import get_column
+import argparse
 
-#print the total number of files in a specified country
-country='United States of America'
-country_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column(file_name,country_column,country,result_column = fires_column)
 
-totalFires = 0
+def get_user_args():
+    """Accepts user input from command line
+    Parameters
+    ----------
+    Returns
+    -------
+    parser
+    arg parser with country, country_column, fires_column, and file_name fields
+    """
+    parser = argparse.ArgumentParser(
+                description='Add parameters for query search')
+    parser.add_argument('--country',
+                        type=str,
+                        help='The name of the query country',
+                        required=True)
+    parser.add_argument('--country_column',
+                        type=int,
+                        help='The index of the column containing countries',
+                        required=True)
+    parser.add_argument('--fires_column',
+                        type=int,
+                        help='The index of the column containing fire data',
+                        required=True)
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='The file name',
+                        required=True)
 
-for fire in fires:
-    totalFires += float(fire)
-print(f'Total Fires in {country} between 1990 and 2020: {totalFires}')
-# print(sum(int(fires)))
+    return parser.parse_args()
+
+
+def main():
+
+    args = get_user_args()
+    fires = get_column(args.file_name, args.country_column,
+                       args.country, result_column=args.fires_column)
+    totalFires = 0
+
+    for fire in fires:
+        totalFires += float(fire)
+    print(f'Total Fires in {args.country} between 1990 and 2020: {totalFires}')
+
+
+if __name__ == "__main__":
+    main()
