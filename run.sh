fb5db6d04ac7ef9ebdbb04ed15ea4225cbd9e3bf
davidgreenblott <david.greenblot@colorado.edu>
2023-09-27 18:11:38 +0000

    remove duplicate files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e # stop on error
-set -u # raise error if variable is unset
-set -o pipefail #fail if any prior step failed
-chmod +x run.sh
-echo "Running print_fires.py"
-python print_fires.py
-
